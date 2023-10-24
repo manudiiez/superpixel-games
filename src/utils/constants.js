34c20a7b9df6a5e7b955bfda90f3a47e0c763974
manolo2829 <manudiez2003@gmail.com>
2023-10-24 16:18:21 -0300

    env deploy data

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const ENV = {
-    SERVER_HOST: "http://localhost:1337",
-    CLIENT_API: "http://localhost:3000/api",
+    SERVER_HOST: "https://superpixel-games-server.up.railway.app",
+    CLIENT_API: "https://superpixel-games-git-main-manudiiezs-projects.vercel.app/api",
     TOKEN: "token",
     CART: "cart",
 }
