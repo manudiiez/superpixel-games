833e311e7dd2b9c4047abd511f8971809212fc05
manolo2829 <manudiez2003@gmail.com>
2023-12-26 10:54:37 -0300

    merge mp constans

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,24 +1,9 @@
-export const ENV = {
-    SERVER_HOST: "http://localhost:1337",
-    CLIENT_API: "http://localhost:3000/api",
-    TOKEN: "token",
-    CART: "cart",
-    API_URL: "http://localhost:1337/api",
-    ENDPOINTS: {
-        USER: 'users',
-        GAME: "games",
-        REGISTER: 'auth/local/register',
-        WISHLIST: 'wishlists',
-        COMMENTS: 'comments'
-    }
-}
-
 // export const ENV = {
-//     SERVER_HOST: "https://superpixel-games-server.up.railway.app",
-//     CLIENT_API: "https://superpixel-games-git-main-manudiiezs-projects.vercel.app/api",
-//     API_URL: "https://superpixel-games-server.up.railway.app/api",
+//     SERVER_HOST: "http://localhost:1337",
+//     CLIENT_API: "http://localhost:3000/api",
 //     TOKEN: "token",
 //     CART: "cart",
+//     API_URL: "http://localhost:1337/api",
 //     ENDPOINTS: {
 //         USER: 'users',
 //         GAME: "games",
@@ -27,3 +12,18 @@ export const ENV = {
 //         COMMENTS: 'comments'
 //     }
 // }
+
+export const ENV = {
+    SERVER_HOST: "https://superpixel-games-server.up.railway.app",
+    CLIENT_API: "https://superpixel-games-muck-git-deploy-manudiiezs-projects.vercel.app/api",
+    API_URL: "https://superpixel-games-server.up.railway.app/api",
+    TOKEN: "token",
+    CART: "cart",
+    ENDPOINTS: {
+        USER: 'users',
+        GAME: "games",
+        REGISTER: 'auth/local/register',
+        WISHLIST: 'wishlists',
+        COMMENTS: 'comments'
+    }
+}
